c5978cac55ad8f23ffbf47adbf46dd64fa9bdaca
Boggs <evan.boggs@mantech.com>
2020-05-13 10:26:34 -0400

    serving the react app with flask file

---
+++ flask-backend/static/react/precache-manifest.5f841bcfe006bd65587d5b317de5dfd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a75f5d477afe01db55afb2173444df9",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "451ac5f832d3ade7d169",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0815c75f4bd43c11a90d",
+    "url": "/static/react/js/2.3f0eea4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.3f0eea4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451ac5f832d3ade7d169",
+    "url": "/static/react/js/main.a8f33fd7.chunk.js"
+  },
+  {
+    "revision": "e4aed13e3c9d3d3550d2",
+    "url": "/static/react/js/runtime-main.3cb41e7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
